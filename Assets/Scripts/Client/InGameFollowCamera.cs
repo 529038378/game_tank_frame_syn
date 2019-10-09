dd11dd04edfe37eb211fa94a8bcbcfca0ddf5774
529038378 <529038378@qq.com>
2019-10-09 13:19:01 +0800

    简单的相机跟随+场景搭建

---
+++ Assets/Scripts/Client/InGameFollowCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameFollowCamera : MonoBehaviour
+{
+    //相机调整参数
+    public int offset_radius = 5;
+    public float head_point = 2.5f;
+    public GameObject m_follow_obj = null;
+    Camera m_cam = null;
+    public float h_degree = 0;
+    public float v_degree = 0;
+    void AdjustCamPos()
+    {
+        if(null != m_cam && null != m_follow_obj)
+        {
+            //计算方向
+            Vector3 dir = new Vector3(Mathf.Cos(Mathf.Deg2Rad * v_degree ) * Mathf.Cos(Mathf.Deg2Rad * h_degree), Mathf.Sin(Mathf.Deg2Rad * v_degree), Mathf.Cos(Mathf.Deg2Rad * v_degree) * Mathf.Sin(Mathf.Deg2Rad * h_degree));
+            m_cam.transform.position = m_follow_obj.transform.position + new Vector3(0, head_point, 0) + offset_radius * dir;
+            m_cam.transform.LookAt(m_follow_obj.transform.position + new Vector3(0, head_point, 0));
+            
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(null != m_follow_obj);
+        m_cam = GetComponent<Camera>();
+        Debug.Assert(m_cam);
+        AdjustCamPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AdjustCamPos();
+    }
+}
---
+++ Assets/Scripts/UI/InGameUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour
+{
+    public GameObject m_player_obj = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_player_obj.transform.Translate(0.0f, 0.0f, 1.0f * Time.deltaTime);
+    }
+
+    private void OnGUI()
+    {
+        Rect op_rect = new Rect(0, UnityEngine.Screen.height - 256, 256, 256);
+        GUILayout.BeginArea(op_rect, "操作区域");
+        if (GUILayout.Button("向前"))
+        {
+            if (null != m_player_obj)
+            {
+                m_player_obj.transform.Translate(0.0f, 0.0f, 1.0f);
+            }
+        }
+        GUILayout.EndArea();
+    }
+}
